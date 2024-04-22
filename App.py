f83df7d063bc7e5ed04c6d90b3cba208fb2a0ec4
Arnab2356 <arnabmanna391@gmail.com>
2024-04-22 13:49:36 +0530

    First commit

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pickle
-import string
+# from sklearn.externals import joblib
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
